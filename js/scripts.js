f4f30a4206aeced7c9830edcc2c473f75ecbde0a
Brooke Fisher <brookenfish1@gmail.com>
2018-11-15 13:44:12 -0500

    Initialized the AJAX request to get the lat and long of the ISS every five seconds

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
 $(function(){
+  console.log('scripts loaded');
 
-  console.log('DOM is loaded');
+  var url = 'http://api.open-notify.org/iss-now.json';
+  var data = [];
+  var lat = '';
+  var long = '';
+  var i = '';
+  var html = '<p>The International Space Station is currently at ';
+
+  function updateFive(){
+    $.ajax({
+      type: 'GET',
+      url: url,
+      dataType: 'json',
+      async: true,
+      data: data,
+      success: function(data){
+        lat = data.iss_position.latitude;
+        long = data.iss_position.longitude;
+        console.log(lat + ' ' + long);
+        html += lat + ' ' + long + '<br/>';
+        $('#results').append(html);
+      }
+    });//end of AJAX
+    setTimeout(updateFive, 5000);
+  }//closing of five second timeout function
+  updateFive();
 
 
-});
+
+
+});//closing of document.ready
