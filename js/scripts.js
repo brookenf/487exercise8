6e1a2328fd85f7d476e4dfe77db2f4a41b7314ea
Brooke Fisher <brookenfish1@gmail.com>
2018-11-16 09:23:19 -0500

    Got my reverse geocode to work

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,13 @@ $(function(){
 
   var url = 'http://api.open-notify.org/iss-now.json';
   var data = [];
+  var geocodeData = [];
   var lat = '';
   var long = '';
   var i = '';
   var html = '<p>The International Space Station is currently at ';
 
+  //UPDATE THE AJAX REQUEST EVERY FIVE SECONDS TO CONTINUOUSLY SEE WHERE THE ISS IS
   function updateFive(){
     $.ajax({
       type: 'GET',
@@ -19,8 +21,23 @@ $(function(){
         lat = data.iss_position.latitude;
         long = data.iss_position.longitude;
         console.log(lat + ' ' + long);
-        html += lat + ' ' + long + '<br/>';
-        $('#results').append(html);
+        var url2 = 'https://nominatim.openstreetmap.org/reverse?format=json&lat=' + lat + '&lon=' + long + '';
+        $.ajax({
+          type: 'GET',
+          dataType: 'json',
+          data: geocodeData,
+          url: url2,
+          async: true,
+          success: function(geocodeData){
+            if(url2 = geocodeData.error){
+              console.log('ISS is over ocean');
+            }else{
+              console.log(geocodeData.address.country);
+            }
+          }
+
+
+        });//closing of reverse geocoding ajax
       }
     });//end of AJAX
     setTimeout(updateFive, 5000);
